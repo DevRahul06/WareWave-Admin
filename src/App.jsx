f1c9f9b0b05726b66185b4a54d36f0b0aa8d0f66
Rahul <rahulchavda6000@gmail.com>
2024-10-27 16:30:24 +0530

    Create Add Product Page , Get All Product Page and Remove Product functionality

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = "₹"
 
 const App = () => {
   const [token, setToken] = useState(
@@ -34,9 +35,9 @@ const App = () => {
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<Add />} token={token} />
-                <Route path="/list" element={<List />} token={token} />
-                <Route path="/orders" element={<Orders />} token={token} />
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
                 <Route />
                 <Route />
               </Routes>
